3533bd5a358646e39cb5d692888aebd21125709c
Pedro Gonçalves <pedrogoncalvesbarbosajunior@gmail.com>
2024-06-07 00:00:45 -0300

    :+1:

---
+++ aula12/pratica006.js
@@ -0,0 +1,9 @@
+var par_ou_impar = 3 // colocar valores para simular que a pessoa digitou algum numero
+
+if (par_ou_impar % 2 == 0) {
+    console.log('É par!')
+}
+
+else{
+    console.log('É ímpar!')
+}
---
+++ aula12/pratica007.js
@@ -0,0 +1,26 @@
+var numero = 3
+
+switch (numero){
+    case 0:
+        console.log(`A fruta correspondente com o número ${numero} é Banana.`)
+        break
+    case 1:
+        console.log(`A fruta correspondente com o número ${numero} é Maça.`)
+        break
+    case 2:
+        console.log(`A fruta correspondente com o número ${numero} é Abacaxi.`)
+        break
+    case 3:
+        console.log(`A fruta correspondente com o número ${numero} é Pera.`)
+        break
+    case 4:
+        console.log(`A fruta correspondente com o número ${numero} é Manga.`)
+        break
+    case 5:
+        console.log(`A fruta correspondente com o número ${numero} é Laranja.`)
+        break
+    default:
+        console.log(`[ERRO] Digito inválido! Por Favor Digite um número entre 0 e 5.`)
+        break
+
+}
