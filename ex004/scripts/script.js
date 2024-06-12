e986977be8dc7e912c4a8adb2af34b3778b1fbd1
Pedro Gonçalves <pedrogoncalvesbarbosajunior@gmail.com>
2024-06-12 14:45:30 -0300

    feito

--- ex004/scripts/script.js
+++ ex004/scripts/script.js
@@ -1,7 +1,7 @@
 
 function verificar(){
 var tabuada = document.getElementById('Itabuada')
-var teste = document.getElementById('teste')
+var txt = document.getElementById('txt')
 var numero = document.querySelector('input[type=number]').value
 var n = 0
 var conta = []
@@ -16,9 +16,9 @@ var l_8 = document.getElementById('l_8')
 var l_9 = document.getElementById('l_9')
 var l_10 = document.getElementById('l_10')
     if (numero == ''){
-        window.alert('[ERRO] Digite um número Válido!')
+        window.alert('[ERRO] Digito inválido! Irá Ser considerada a Tabuada do número 0.')
     }
-
+    
     if (Number(numero) <= 0 || Number(numero) > 0){
         while (n <= 9){
             n += 1
@@ -26,15 +26,16 @@ var l_10 = document.getElementById('l_10')
         }
     }
 
-    l_1.innerHTML = `${numero} x ${conta[0]} = ${numero * conta[0]}`
-    l_2.innerHTML = `${numero} x ${conta[1]} = ${numero * conta[1]}`
-    l_3.innerHTML = `${numero} x ${conta[2]} = ${numero * conta[2]}`
-    l_4.innerHTML = `${numero} x ${conta[3]} = ${numero * conta[3]}`
-    l_5.innerHTML = `${numero} x ${conta[4]} = ${numero * conta[4]}`
-    l_6.innerHTML = `${numero} x ${conta[5]} = ${numero * conta[5]}`
-    l_7.innerHTML = `${numero} x ${conta[6]} = ${numero * conta[6]}`
-    l_8.innerHTML = `${numero} x ${conta[7]} = ${numero * conta[7]}`
-    l_9.innerHTML = `${numero} x ${conta[8]} = ${numero * conta[8]}`
-    l_10.innerHTML = `${numero} x ${10} = ${numero * 10}`
+    l_1.innerHTML = `${numero.replace('', '0')} x ${conta[0]} = ${numero * conta[0]}`
+    l_2.innerHTML = `${numero.replace('', '0')} x ${conta[1]} = ${numero * conta[1]}`
+    l_3.innerHTML = `${numero.replace('', '0')} x ${conta[2]} = ${numero * conta[2]}`
+    l_4.innerHTML = `${numero.replace('', '0')} x ${conta[3]} = ${numero * conta[3]}`
+    l_5.innerHTML = `${numero.replace('', '0')} x ${conta[4]} = ${numero * conta[4]}`
+    l_6.innerHTML = `${numero.replace('', '0')} x ${conta[5]} = ${numero * conta[5]}`
+    l_7.innerHTML = `${numero.replace('', '0')} x ${conta[6]} = ${numero * conta[6]}`
+    l_8.innerHTML = `${numero.replace('', '0')} x ${conta[7]} = ${numero * conta[7]}`
+    l_9.innerHTML = `${numero.replace('', '0')} x ${conta[8]} = ${numero * conta[8]}`
+    l_10.innerHTML = `${numero.replace('', '0')} x ${10} = ${numero * 10}`
+    txt.innerHTML = `O Número ${numero.replace('', '0')} foi identificado com sucesso!`
 }
 
