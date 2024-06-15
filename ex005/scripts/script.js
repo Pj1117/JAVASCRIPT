53a1bed331f28ba2d4b7fee7b2763e12978d64f0
Pedro Gonçalves <pedrogoncalvesbarbosajunior@gmail.com>
2024-06-14 23:13:01 -0300

    .

---
+++ ex005/scripts/script.js
@@ -0,0 +1,23 @@
+var lista = []
+var numero = document.getElementById('Inumero')
+n = Number(numero.value)
+
+var res = document.querySelector('div#res')
+
+
+function adicionar(number){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
