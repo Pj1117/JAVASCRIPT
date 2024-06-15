379bf7f54b4ea519f137dc35551eafb3ac5ad4a0
Pedro Gonçalves <pedrogoncalvesbarbosajunior@gmail.com>
2024-06-14 23:45:05 -0300

    ....

--- ex005/scripts/script.js
+++ ex005/scripts/script.js
@@ -1,13 +1,19 @@
 var lista = []
 var numero = document.getElementById('Inumero')
-n = Number(numero.value)
-
 var res = document.querySelector('div#res')
-
-
-function adicionar(number){
-    
+function adicionar(){
+    if (n > 100 || n < 0){
+        window.alert ('Por favor digite um número válido!')
+    } else{
+        lista.push(n)
+        return res.innerHTML = `Valor ${n} adicionado!`
+    }
 }
+
+
+
+
+
 
 
 
