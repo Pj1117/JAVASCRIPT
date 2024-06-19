9bd5060da145d37861122fbaf4dc9024311f486a
Pedro Gonçalves <pedrogoncalvesbarbosajunior@gmail.com>
2024-06-19 20:10:22 -0300

    corrigir

--- ex005/scripts/script.js
+++ ex005/scripts/script.js
@@ -1,37 +1,48 @@
 // tentar criar option no select
-var amarelo = []
+var lista_definitiva = []
 var lista = []
-
-var teste = document.getElementById('teste')
+var div_fializar = document.getElementById('final')
+var botao_final = document.getElementById('botao')
+var total = 0
+botao_final.style.display = 'none'
 function adicionar (numero){
     var select = document.getElementById('select')
     if (numero  > 0 && numero <= 100){
-        c = 0
+        botao_final.style.display = 'inline'
+        let c = 0
         while (lista.includes(numero) == false){
         let criar = document.createElement('option')
         select.appendChild(criar)
             lista += numero
-            amarelo.push(numero)
+            total += Number(numero)
+            lista_definitiva.push(numero)
             c += 1
             return criar.innerHTML = `Valor ${numero} adicionado!`
         }
-    } else if (amarelo.includes(numero) != -1){
-        return window.alert(`${amarelo} Número já adicionado!`)
-    } else if (numero < 0 || numero > 100 || numero.lenght == 0){
+    } else if (lista_definitiva.includes(numero) == true){
+        return window.alert(`${lista_definitiva} Número já adicionado!`)
+    } else if (numero <= 0 || numero > 100 || numero.lenght == 0){
         return window.alert (`O número ${numero} não é um número que está entre 1 e 100!`)
     }
 }
 
 
-function soma(numero){
-
+function soma(){
+    let criar_p0 = document.createElement('p')
+    div_fializar.appendChild(criar_p0)
+    return criar_p0.innerHTML = `A soma de todos os números digitados é ${total}`
 }
 
-function menor(numero){
-    // criar elemento e um pai nas funçoes do finalizar
+function menor(){
+    let criar_p1 = document.createElement('p')
+    div_fializar.appendChild(criar_p1)
+    var lista_com_sort = lista_definitiva.sort()
+    return criar_p1.innerHTML = `O menor número digitado foi ${lista_definitiva[0]}`
 }
 
-function media(numero){
-
+function media(){
+    let criar_p2 = document.createElement('p')
+    div_fializar.appendChild(criar_p2)
+    return criar_p2.innerHTML = `A media dos números digitados é ${total/lista.length}`
 }
 
