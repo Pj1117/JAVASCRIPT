4add41497e208f4d3de804de04af5b337928e0f4
Pedro Gonçalves <pedrogoncalvesbarbosajunior@gmail.com>
2024-06-18 23:47:00 -0300

    .

--- ex005/scripts/script.js
+++ ex005/scripts/script.js
@@ -1,29 +1,44 @@
-lista = []
-var numero = document.getElementById('Inumero')
-var res = document.querySelector('div#res')
-function adicionar(){
-    if (n > 100 || n < 0){
-        window.alert ('Por favor digite um número válido!')
-    } else{
-        lista.push(n)
-        return res.innerHTML = `Valor ${n} adicionado!`
+// tentar criar option no select
+
+function adicionar (numero){
+    var select = document.getElementById('select')
+    if (numero  > 0 && numero <= 100){
+       let criar = document.createElement('option')
+       select.appendChild(criar)
+       return criar.innerHTML = `teste ${numero}`
+    } else if (numero < 0 || numero > 100 || numero.lenght == 0){
+        return alert('testando')
+    }
+}
+
+function lista(numero){
+    var teste = document.getElementById('teste')
+    var lista = []
+    var amarelo = []
+    c = 0
+    if (numero > 0 && numero <= 100){
+        while (numero > 0 && numero <= 100){
+            lista.push(numero)
+            c += 1
+            amarelo.push(lista)
+            return teste.innerHTML += `Lista: ${lista[c-1]} e ${numero} e ${c}<br>
+            amarelo ${amarelo}`
+        }
     }
 }
 
 
+function soma(numero){
+
+}
+
+function menor(numero){
+    // criar elemento e um pai nas funçoes do finalizar
+}
+
+function media(numero){
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// var teste = document.getElementById('teste')
